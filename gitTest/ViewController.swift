86a13b0e8379653f8dba38fa1ca586e4390fcddd
takata142 <takata.ikuo@t-denso.com>
2021-11-17 11:13:19 +0900

    first commit

--- gitTest/ViewController.swift
+++ gitTest/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label1: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        label1.text = "lalabel"
     }
 
 
