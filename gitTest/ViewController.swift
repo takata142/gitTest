9626c6e1a0689c7e360600c96585ec7b838e8162
takata142 <takata.ikuo@t-denso.com>
2021-11-17 16:21:35 +0900

    webview,statusbarstyle->white

--- gitTest/ViewController.swift
+++ gitTest/ViewController.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var view1: UIView!
+    
+    var webView: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        label1.text = "lalabel"
+        
+        //let v = CGRect(x: 0, y: 0, width: 414, height: 862)
+        
+        //WKWebViewを生成
+        webView = WKWebView(frame: CGRect(x: 0, y: 36, width: 414, height: 862))
+        //WKWebViewをViewControllerのviewに追加する
+        view.addSubview(webView)
+        //リクエストを生成
+        let request = URLRequest(url: URL(string: "https://developer.apple.com/")!)
+        webView.load(request)
     }
 
 
